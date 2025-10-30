b6e1c0ed27784f16f8cc0649bebca2549bd8d4cf
Judit <jplanasc@users.noreply.github.com>
2025-10-30 17:00:31 +0100

    Split morphology and spines (#1)
    * Split morphology and spines
    
    Going towards a separation of different conceptual classes:
    morphology, soma, spines and a container that groups all together.
    
    Several modifications have been made, but further improvements
    need to be done.
    
    * Add usage examples
    
    * Refined examples
    
    * Add scripts to simplify meshes
    
    * Add sample data for the examples
    
    * Rename variables referring to HDF5 group structure
    
    * Remove Morphology class: use NeuroM's Morphology instead
    
    * Rename SpinesOnly class to Spines
    
    * Use f-strings whenever possible
    
    * Improved mesh manipulation scripts
    
    * Improve code and rename variables for readability
    
    * More variable renaming for better readability
    
    * Project reorganization to fit src layout
    
    * First conceptual division of code
    
    Split the code according to the following concepts:
    - Morphology-with-spines data class
    - Morphology class (from NeuroM)
    - Soma class
    - Spines class
    - HDF5 file format structure
    
    * Update README.md

--- morph_spines/__init__.py
+++
@@ -1,9 +0,0 @@
-"""morph_spines."""
-
-from importlib.metadata import version
-__version__ = version(__package__)
-
-from neurom.io.utils import MorphLoader, load_morphologies, load_morphology
-
-from .morphology_with_spines import MorphologyWithSpines
-from .morph_spine_loader import load_morphology_with_spines
