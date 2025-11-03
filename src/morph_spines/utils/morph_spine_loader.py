47d443eefbae33d76014ae444d512ea7279e2dac
Michael W. Reimann <michael.reimann@openbraininstitute.org>
2025-11-03 11:31:17 +0100

    morphology name resolution now in standalone function.

--- src/morph_spines/utils/morph_spine_loader.py
+++ src/morph_spines/utils/morph_spine_loader.py
@@ -11,6 +11,21 @@ from morph_spines.core.soma import Soma
 from morph_spines.core.spines import Spines
 
 
+def _resolve_morphology_name(
+        morphology_filepath, morphology_name
+):
+    with h5py.File(morphology_filepath, "r") as h5:
+        lst_morph_names = list(h5[GRP_MORPH].keys())
+        if len(lst_morph_names) == 0:
+            raise ValueError("The file is not a valid morphology-with-spines file")
+        if morphology_name is None:
+            if len(lst_morph_names) > 1:
+                raise ValueError("Multiple morphology names found in the file: must specify a morphology name")
+            morphology_name = lst_morph_names[0]
+        if morphology_name not in lst_morph_names:
+            raise ValueError(f"Morphology {morphology_name} not found in file")
+    return morphology_name
+
 def load_morphology_with_spines(
     morphology_filepath, morphology_name=None, spines_are_centered=True, process_subtrees=False
 ):
@@ -18,42 +33,26 @@ def load_morphology_with_spines(
     Loads a neuron morphology with spines from a hdf5 archive.
     Returns the representation of a spiny morphology of this package.
     """
+    morphology_name = _resolve_morphology_name(morphology_filepath, morphology_name)
     morphology = load_morphology(morphology_filepath, morphology_name, process_subtrees)
-    morphology_name = morphology.name
     soma = Soma(morphology_filepath, morphology_name)
     spines = load_spines(morphology_filepath, morphology_name, spines_are_centered)
     return MorphologyWithSpines(morphology, soma, spines)
 
-def load_morphology(filepath, name=None, process_subtrees=False):
+def load_morphology(filepath, name, process_subtrees=False):
     """Load a neuron morphology from a neuron morphology with spines representation.
     Loads the basic neuron morphology without its spine representation.
     Returns the representation of the neuron morphology.
     """
-    with h5py.File(filepath, "r") as h5:
-        lst_morph_names = list(h5[GRP_MORPH].keys())
-        if len(lst_morph_names) == 0:
-            raise ValueError("The file is not a valid morphology-with-spines file")
-        if name is None:
-            if len(lst_morph_names) > 1:
-                raise ValueError("Multiple morphology names found in the file: must specify a morphology name")
-            name = lst_morph_names[0]
-        if name not in lst_morph_names:
-            raise ValueError(f"Morphology {name} not found in file")
-
     coll = morphio.Collection(filepath)
     morphology = coll.load(f"{GRP_MORPH}/{name}")
     return Morphology(morphology, name, process_subtrees=process_subtrees)
 
-def load_spines(filepath, name=None, spines_are_centered=True):
+def load_spines(filepath, name, spines_are_centered=True):
     """Load the spines from a neuron morphology with spines representation.
     Loads the spines of a 'neuron morphology with spines' from a hdf5 archive.
     Returns the representation of the spines.
     """
-    with h5py.File(filepath, "r") as h5:
-        lst_morph_names = list(h5[GRP_EDGES].keys())
-        if name not in lst_morph_names:
-            raise ValueError(f"Morphology {name} not found in file!")
-
     spine_table = pandas.read_hdf(filepath, key=f"{GRP_EDGES}/{name}")
     coll = morphio.Collection(filepath)
     centered_spine_skeletons = neurom_load_morphology(
