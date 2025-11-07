a930a51a6c14e249889ae0458ad44b3e3548ebaf
Judit Planas <judit.planas@openbraininstitute.org>
2025-11-07 17:02:14 +0100

    Add functionality to create deterministic sample data and use it for testing

--- scripts/create_sample_data.py
+++ scripts/create_sample_data.py
@@ -1,10 +1,10 @@
 """This script creates a morphology-with-spines file with fake sample data."""
 
+from pathlib import Path
+
 import h5py
 import numpy as np
 import pandas as pd
-from pathlib import Path
-
 
 morphology_filename = "morph_with_spines_schema.h5"
 morphology_name = "01234"
@@ -13,44 +13,89 @@ output_dir.mkdir(exist_ok=True)
 output_file = Path(f"{output_dir}/{morphology_filename}")
 
 # Group /edges
-#edges_grp = h5_file.create_group("edges")
-#edges_id = edges_grp.create_group(morphology_name)
+# edges_grp = h5_file.create_group("edges")
+# edges_id = edges_grp.create_group(morphology_name)
 
 # Pandas dataframe columns (axis0)
-columns = ['afferent_surface_x', 'afferent_surface_y', 'afferent_surface_z',
-           'afferent_center_x', 'afferent_center_y', 'afferent_center_z',
-           'spine_id', 'spine_morphology', 'spine_length',
-           'spine_orientation_vector_x', 'spine_orientation_vector_y',
-           'spine_orientation_vector_z', 'spine_rotation_x', 'spine_rotation_y',
-           'spine_rotation_z', 'spine_rotation_w', 'afferent_section_id',
-           'afferent_segment_id', 'afferent_segment_offset',
-           'afferent_section_pos']
+columns = [
+    "afferent_surface_x",
+    "afferent_surface_y",
+    "afferent_surface_z",
+    "afferent_center_x",
+    "afferent_center_y",
+    "afferent_center_z",
+    "spine_id",
+    "spine_morphology",
+    "spine_length",
+    "spine_orientation_vector_x",
+    "spine_orientation_vector_y",
+    "spine_orientation_vector_z",
+    "spine_rotation_x",
+    "spine_rotation_y",
+    "spine_rotation_z",
+    "spine_rotation_w",
+    "afferent_section_id",
+    "afferent_segment_id",
+    "afferent_segment_offset",
+    "afferent_section_pos",
+]
 
 # Number of spines (axis1)
 num_spines = 2
 
-df = pd.DataFrame({
-    'afferent_surface_x':           np.random.random(num_spines),
-    'afferent_surface_y':           np.random.random(num_spines),
-    'afferent_surface_z':           np.random.random(num_spines),
-    'afferent_center_x':            np.random.random(num_spines),
-    'afferent_center_y':            np.random.random(num_spines),
-    'afferent_center_z':            np.random.random(num_spines),
-    'spine_id':                     np.array(range(num_spines)),
-    'spine_morphology':             [morphology_name] * num_spines,
-    'spine_length':                 np.random.random(num_spines),
-    'spine_orientation_vector_x':   np.random.random(num_spines),
-    'spine_orientation_vector_y':   np.random.random(num_spines),
-    'spine_orientation_vector_z':   np.random.random(num_spines),
-    'spine_rotation_x':             np.random.random(num_spines),
-    'spine_rotation_y':             np.random.random(num_spines),
-    'spine_rotation_z':             np.random.random(num_spines),
-    'spine_rotation_w':             np.random.random(num_spines),
-    'afferent_section_id':          np.random.random(num_spines),
-    'afferent_segment_id':          np.random.random(num_spines),
-    'afferent_segment_offset':      np.random.random(num_spines),
-    'afferent_section_pos':         np.random.random(num_spines)
-})
+random_df_data = False
+
+if random_df_data:
+    df = pd.DataFrame(
+        {
+            "afferent_surface_x": np.random.random(num_spines),
+            "afferent_surface_y": np.random.random(num_spines),
+            "afferent_surface_z": np.random.random(num_spines),
+            "afferent_center_x": np.random.random(num_spines),
+            "afferent_center_y": np.random.random(num_spines),
+            "afferent_center_z": np.random.random(num_spines),
+            "spine_id": np.array(range(num_spines), dtype=np.int64),
+            "spine_morphology": [morphology_name] * num_spines,
+            "spine_length": np.random.random(num_spines),
+            "spine_orientation_vector_x": np.random.random(num_spines),
+            "spine_orientation_vector_y": np.random.random(num_spines),
+            "spine_orientation_vector_z": np.random.random(num_spines),
+            "spine_rotation_x": np.random.random(num_spines),
+            "spine_rotation_y": np.random.random(num_spines),
+            "spine_rotation_z": np.random.random(num_spines),
+            "spine_rotation_w": np.random.random(num_spines),
+            "afferent_section_id": np.random.randint(num_spines),
+            "afferent_segment_id": np.random.randint(num_spines),
+            "afferent_segment_offset": np.random.random(num_spines),
+            "afferent_section_pos": np.random.random(num_spines),
+        }
+    )
+else:
+    spines = np.array(range(num_spines), dtype=np.int64) + 1
+    df = pd.DataFrame(
+        {
+            "afferent_surface_x": np.array([0.1 * i for i in spines], dtype=np.float64),
+            "afferent_surface_y": np.array([0.1 * i + i / 100 for i in spines], dtype=np.float64),
+            "afferent_surface_z": np.array([0.1 * i + i / 1000 for i in spines], dtype=np.float64),
+            "afferent_center_x": np.array([1.0 * i + i for i in spines], dtype=np.float64),
+            "afferent_center_y": np.array([1.0 * i + i * 10 for i in spines], dtype=np.float64),
+            "afferent_center_z": np.array([1.0 * i + i * 100 for i in spines], dtype=np.float64),
+            "spine_id": np.array(range(num_spines), dtype=np.int64),
+            "spine_morphology": [morphology_name] * num_spines,
+            "spine_length": np.array([i for i in spines], dtype=np.float64),
+            "spine_orientation_vector_x": np.array([0.1234 * i for i in spines], dtype=np.float64),
+            "spine_orientation_vector_y": np.array([0.2345 * i for i in spines], dtype=np.float64),
+            "spine_orientation_vector_z": np.array([0.3456 * i for i in spines], dtype=np.float64),
+            "spine_rotation_x": np.array([0.4567 * i for i in spines], dtype=np.float64),
+            "spine_rotation_y": np.array([0.5678 * i for i in spines], dtype=np.float64),
+            "spine_rotation_z": np.array([0.6789 * i for i in spines], dtype=np.float64),
+            "spine_rotation_w": np.array([0.7891 * i for i in spines], dtype=np.float64),
+            "afferent_section_id": np.array([10 + i for i in spines], dtype=np.int64),
+            "afferent_segment_id": np.array([100 + i for i in spines], dtype=np.int64),
+            "afferent_segment_offset": np.array([0.8901 * i for i in spines], dtype=np.float64),
+            "afferent_section_pos": np.array([0.9012 * i for i in spines], dtype=np.float64),
+        }
+    )
 
 key = str(f"/edges/{morphology_name}")
 
@@ -61,18 +106,21 @@ with h5py.File(output_file, "a") as h5_file:
     morph_grp = h5_file.create_group("morphology")
     morph_id = morph_grp.create_group(morphology_name)
 
-    morph_points = np.array([
-        [0, 0, 0, 1],
-        [1, 1, 1, 1],
-        [2, 2, 2, 4],
-        [3, 2, 3, 4],
-        [3, 2, 3, 4],
-        [4, 3, 3, 4],
-        [3, 2, 3, 5],
-        [5, 5, 5, 5]
-    ], dtype=np.float32)
+    morph_points = np.array(
+        [
+            [0, 0, 0, 1],
+            [1, 1, 1, 1],
+            [2, 2, 2, 4],
+            [3, 2, 3, 4],
+            [3, 2, 3, 4],
+            [4, 3, 3, 4],
+            [3, 2, 3, 5],
+            [5, 5, 5, 5],
+        ],
+        dtype=np.float32,
+    )
 
-    morph_structure = np.array([ [0, 1, -1], [2, 2, 0], [4, 2, 1], [6, 2, 1] ], dtype=np.int32)
+    morph_structure = np.array([[0, 1, -1], [2, 2, 0], [4, 2, 1], [6, 2, 1]], dtype=np.int32)
 
     morph_id.create_dataset("points", data=morph_points)
     morph_id.create_dataset("structure", data=morph_structure)
@@ -83,8 +131,10 @@ with h5py.File(output_file, "a") as h5_file:
     soma_id = soma_meshes.create_group(morphology_name)
 
     # Create a triangular bipyramid (2 inverted pyramids, sharing its base)
-    triangles = np.array([ [0, 1, 2], [0, 2, 3], [0, 3, 4], [0, 4, 1], [5, 2, 1], [5, 3, 2], [5, 4, 3], [5, 1, 4] ])
-    vertices = np.array([ [0, 0, 1], [1, 0, 0], [0, 1, 0], [-1, 0, 0], [0, -1, 0], [0, 0, -1] ])
+    triangles = np.array(
+        [[0, 1, 2], [0, 2, 3], [0, 3, 4], [0, 4, 1], [5, 2, 1], [5, 3, 2], [5, 4, 3], [5, 1, 4]]
+    )
+    vertices = np.array([[0, 0, 1], [1, 0, 0], [0, 1, 0], [-1, 0, 0], [0, -1, 0], [0, 0, -1]])
     soma_id.create_dataset("triangles", data=triangles)
     soma_id.create_dataset("vertices", data=vertices)
 
@@ -95,21 +145,55 @@ with h5py.File(output_file, "a") as h5_file:
     spines_meshes = spines_grp.create_group("meshes")
     spines_id = spines_meshes.create_group(morphology_name)
 
-    offsets = np.array([
-        [0, 0], [5, 6], [9, 10]     # [vertices_offset, triangles_offset]
-    ])
-    triangles = np.array([
-        #[0, 1, 2], [0, 2, 3], [0, 3, 4], [0, 4, 1], [1, 3, 2], [1, 4, 3], # square-based pyramid shaped spine, center at (0, 0, 0)
-        #[0, 2, 1], [0, 3, 2], [0, 1, 3], [1, 2, 3]                        # triangle-based pyramid shaped spine, center at (0, 0, 0)
-        [0, 2, 1], [0, 3, 2], [0, 4, 3], [0, 1, 4], [1, 2, 3], [1, 3, 4],  # square-based pyramid shaped spine, apex at (2, 2, 2)
-        [0, 1, 2], [0, 2, 3], [0, 3, 1], [1, 3, 2]                         # triangle-based pyramid shaped spine, apex at (5, 5, 5)
-    ])
-    vertices = np.array([
-        #[0, 0, 2], [1, 0, 0], [0, 1, 0], [-1, 0, 0], [0, -1, 0], # square-based pyramid shaped spine, center at (0, 0, 0)
-        #[0, 0, 4], [0, 2, 0], [2, -2, 0], [-2, -2, 0]            # triangle-based pyramid shaped spine, center at (0, 0, 0)
-        [2, 2, 2], [3, 2, 4], [2, 3, 4], [1, 2, 4], [2, 1, 4],    # square-based pyramid shaped spine, apex at (2, 2, 2)
-        [5, 5, 5], [5, 7, 9], [7, 3, 9], [3, 3, 9]                # triangle-based pyramid shaped spine, apex at (5, 5, 5)
-    ])
+    # Offset format: [vertices_offset, triangles_offset]
+    offsets = np.array(
+        [
+            [0, 0],
+            [5, 6],
+            [9, 10],
+        ]
+    )
+
+    # Triangles-vertices pair examples:
+    # square-based pyramid shaped spine, center at (0, 0, 0):
+    #   triangles = [ [0, 1, 2], [0, 2, 3], [0, 3, 4], [0, 4, 1], [1, 3, 2], [1, 4, 3] ]
+    #   vertices = [ [0, 0, 2], [1, 0, 0], [0, 1, 0], [-1, 0, 0], [0, -1, 0] ]
+    # triangle-based pyramid shaped spine, center at (0, 0, 0)
+    #   triangles = [ [0, 2, 1], [0, 3, 2], [0, 1, 3], [1, 2, 3] ]
+    #   vertices = [ [0, 0, 4], [0, 2, 0], [2, -2, 0], [-2, -2, 0] ]
+    # square-based pyramid shaped spine, apex at (2, 2, 2)
+    #   triangles = [ [0, 2, 1], [0, 3, 2], [0, 4, 3], [0, 1, 4], [1, 2, 3], [1, 3, 4] ]
+    #   vertices = [ [2, 2, 2], [3, 2, 4], [2, 3, 4], [1, 2, 4], [2, 1, 4] ]
+    # triangle-based pyramid shaped spine, apex at (5, 5, 5)
+    #   triangles = [ [0, 1, 2], [0, 2, 3], [0, 3, 1], [1, 3, 2] ]
+    #   vertices = [ [5, 5, 5], [5, 7, 9], [7, 3, 9], [3, 3, 9] ]
+    triangles = np.array(
+        [
+            [0, 2, 1],
+            [0, 3, 2],
+            [0, 4, 3],
+            [0, 1, 4],
+            [1, 2, 3],
+            [1, 3, 4],  # end of square-based pyramid shaped spine, apex at (2, 2, 2)
+            [0, 1, 2],
+            [0, 2, 3],
+            [0, 3, 1],
+            [1, 3, 2],  # end of triangle-based pyramid shaped spine, apex at (5, 5, 5)
+        ]
+    )
+    vertices = np.array(
+        [
+            [2, 2, 2],
+            [3, 2, 4],
+            [2, 3, 4],
+            [1, 2, 4],
+            [2, 1, 4],  # end of square-based pyramid shaped spine, apex at (2, 2, 2)
+            [5, 5, 5],
+            [5, 7, 9],
+            [7, 3, 9],
+            [3, 3, 9],  # end of triangle-based pyramid shaped spine, apex at (5, 5, 5)
+        ]
+    )
 
     spines_id.create_dataset("offsets", data=offsets)
     spines_id.create_dataset("triangles", data=triangles)
@@ -119,17 +203,16 @@ with h5py.File(output_file, "a") as h5_file:
     spines_skel = spines_grp.create_group("skeletons")
     spines_skel_id = spines_skel.create_group(morphology_name)
 
-    points = np.array([
-        [2, 2, 2, 0.1], # spine 0 start (x, y, z, d)
-        [2, 2, 4, 2.0], # spine 0 end (x, y, z, d)
-        [5, 5, 5, 0.1], # spine 1 start (x, y, z, d)
-        [5, 5, 9, 2.0]  # spine 1 end (x, y, z, d)
-    ])
+    points = np.array(
+        [
+            [2, 2, 2, 0.1],  # spine 0 start (x, y, z, d)
+            [2, 2, 4, 2.0],  # spine 0 end (x, y, z, d)
+            [5, 5, 5, 0.1],  # spine 1 start (x, y, z, d)
+            [5, 5, 9, 2.0],  # spine 1 end (x, y, z, d)
+        ]
+    )
 
-    structure = np.array([
-        [2, 2, -1],
-        [5, 2, -1]
-    ])
+    structure = np.array([[2, 2, -1], [5, 2, -1]])
 
     spines_skel_id.create_dataset("points", data=points)
     spines_skel_id.create_dataset("structure", data=structure)
--- tests/unit/core/test_spines.py
+++ tests/unit/core/test_spines.py
@@ -1,80 +1,99 @@
-import pytest
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
-from pathlib import Path
-import h5py
+import pytest
 import trimesh
 from numpy.ma.testutils import assert_array_equal
-from scipy.spatial.transform import Rotation
 
-#from NeuroM.neurom.core.morphology import Neurite
-from morph_spines.core.h5_schema import (
-    COL_ROTATION, COL_TRANSLATION, COL_SPINE_MESH, COL_SPINE_ID,
-    COL_AFF_SEC, GRP_SPINES, GRP_MESHES, GRP_VERTICES, GRP_TRIANGLES,
-    GRP_OFFSETS
-)
-
+# from NeuroM.neurom.core.morphology import Neurite
 from morph_spines.core.spines import Spines
 
-
 SAMPLE_DATA_DIR = f"{Path(__file__).parent.parent}/data"
-SAMPLE_MORPH_WITH_SPINES_FILE= f"{SAMPLE_DATA_DIR}/morph_with_spines_schema.h5"
+SAMPLE_MORPH_WITH_SPINES_FILE = f"{SAMPLE_DATA_DIR}/morph_with_spines_schema.h5"
 MORPH_WITH_SPINES_ID = "01234"
 
 
 @pytest.fixture
 def spines():
     """Fixture providing a Spines instance"""
-    spine_table = pd.read_hdf(SAMPLE_MORPH_WITH_SPINES_FILE, key=str(f"/edges/{MORPH_WITH_SPINES_ID}"))
+    spine_table = pd.read_hdf(
+        SAMPLE_MORPH_WITH_SPINES_FILE, key=str(f"/edges/{MORPH_WITH_SPINES_ID}")
+    )
     return Spines(
         meshes_filepath=SAMPLE_MORPH_WITH_SPINES_FILE,
         morphology_name=MORPH_WITH_SPINES_ID,
         spine_table=spine_table,
         centered_spine_skeletons=False,
-        spines_are_centered=False
+        spines_are_centered=False,
     )
-
 
 
 def test_spine_count(spines):
     assert spines.spine_count == 2
 
+
 def test_spine_transformations(spines):
     expected_transformations = (
-        np.array([
-            [0.44180798, -0.58406595, 0.68093515],
-            [0.64080507, 0.73666259, 0.21609509],
-            [-0.62783323, 0.34087416, 0.69973583]
-        ]),
-        np.array([0.926668, 0.0630049, 0.957736]))
+        np.array(
+            [
+                [0.02969959, -0.3423923, 0.93908756],
+                [0.98483344, 0.17069435, 0.03108899],
+                [-0.17094157, 0.92392151, 0.34226894],
+            ]
+        ),
+        np.array([0.1, 0.11, 0.101]),
+    )
     transformations = spines.spine_transformations(0)
-    assert np.allclose(transformations[0].as_matrix(), expected_transformations[0], rtol=1e-6, atol=1e-8)
-    assert np.allclose(transformations[1], expected_transformations[1], rtol=1e-6, atol=1e-8)
+    assert len(transformations[0].as_matrix()) == len(expected_transformations[0])
+    assert len(transformations[1]) == len(expected_transformations[1])
 
-#def test_spine_skeletons(spines):
+    assert np.allclose(
+        transformations[0].as_matrix(),
+        expected_transformations[0],  # , rtol=1e-5, atol=1e-7
+    )
+    assert np.allclose(transformations[1], expected_transformations[1])  # , rtol=1e-6, atol=1e-7)
+
+
+# def test_spine_skeletons(spines):
 #    spine_skeletons = spines.spine_skeletons
 #    assert len(spine_skeletons) == 2
 
+
 def test__spine_mesh_points(spines):
-    expected_points = np.array([[2, 2, 2,], [3, 2, 4,], [2, 3, 4], [1, 2, 4], [2, 1, 4]])
+    expected_points = np.array(
+        [
+            [2, 2, 2],
+            [3, 2, 4],
+            [2, 3, 4],
+            [1, 2, 4],
+            [2, 1, 4],
+        ]
+    )
     points = spines._spine_mesh_points(spine_loc=0, transform=False)
     assert_array_equal(points, expected_points)
 
+
 def test_spine_mesh_triangles(spines):
-    expected_triangles = np.array([[0, 2, 1], [0, 3, 2], [0, 4, 3], [0, 1, 4], [1, 2, 3], [1, 3, 4]])
+    expected_triangles = np.array(
+        [[0, 2, 1], [0, 3, 2], [0, 4, 3], [0, 1, 4], [1, 2, 3], [1, 3, 4]]
+    )
     triangles = spines.spine_mesh_triangles(spine_loc=0)
     assert_array_equal(triangles, expected_triangles)
+
 
 def test_spine_mesh(spines):
     mesh = spines.spine_mesh(0)
     assert isinstance(mesh, trimesh.Trimesh)
 
+
 def test_centered_spine_mesh(spines):
     mesh = spines.centered_spine_mesh(0)
     assert isinstance(mesh, trimesh.Trimesh)
 
-#def test_spine_indices_for_section(spines):
+
+# def test_spine_indices_for_section(spines):
 #    spines.spine_indices_for_section(0)
 
-#def spine_table_for_section(self, section_id):
+# def spine_table_for_section(self, section_id):
 #    spines.spine_table_for_section(0)
