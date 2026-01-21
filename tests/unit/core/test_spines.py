d9436533e1200b4881079ac8888f1e4b84a571e3
Judit Planas <judit.planas@openbraininstitute.org>
2026-01-21 11:27:00 +0100

    Add more tests for spines

--- tests/unit/core/test_spines.py
+++ tests/unit/core/test_spines.py
@@ -6,7 +6,9 @@ import pandas as pd
 import pytest
 import trimesh
 from neurom import load_morphology as neurom_load_morphology
+from neurom.core.morphology import Neurite
 from numpy.ma.testutils import assert_array_equal
+from numpy.testing import assert_allclose
 
 from morph_spines.core.spines import Spines
 
@@ -34,6 +36,13 @@ def spines():
         centered_spine_skeletons=spines_skeletons,
         spines_are_centered=True,
     )
+
+
+@pytest.fixture
+def spines_meshes(spines):
+    """Fixture providing a Spines instance to test meshes"""
+    spines.spine_table["afferent_section_id"] = [2, 2]
+    return spines
 
 
 def test_spine_count(spines):
@@ -69,9 +78,24 @@ def test__transform_spine_skeletons_fail_num_spines(spines):
         spines._transform_spine_skeletons()
 
 
-# def test_spine_skeletons(spines):
-#    spine_skeletons = spines.spine_skeletons
-#    assert len(spine_skeletons) == 2
+def test_spine_skeletons(spines):
+    spine_skeletons = spines.spine_skeletons
+    expected_points = np.array([[2.0, 2.0, 3.0, 0.05], [2.0, 2.0, 5.0, 1.0]], dtype=np.float32)
+
+    assert len(spine_skeletons) == 2
+    assert isinstance(spine_skeletons[0], Neurite)
+    assert spine_skeletons[0].length == 2.0
+    assert_allclose(spine_skeletons[0].points, expected_points)
+
+
+def test_centered_spine_skeletons(spines):
+    spine_skeletons = spines.centered_spine_skeletons
+    expected_points = np.array([[0.0, 0.0, 0.0, 0.05], [0.0, 0.0, 2.0, 1.0]], dtype=np.float32)
+
+    assert len(spine_skeletons) == 2
+    assert isinstance(spine_skeletons[0], Neurite)
+    assert spine_skeletons[0].length == 2.0
+    assert_allclose(spine_skeletons[0].points, expected_points)
 
 
 def test__spine_mesh_points(spines):
@@ -106,8 +130,75 @@ def test_centered_spine_mesh(spines):
     assert isinstance(mesh, trimesh.Trimesh)
 
 
-# def test_spine_indices_for_section(spines):
-#    spines.spine_indices_for_section(0)
+def test_spine_indices_for_section(spines):
+    expected_indices = [0]
+    indices = spines.spine_indices_for_section(2)
 
-# def spine_table_for_section(self, section_id):
-#    spines.spine_table_for_section(0)
+    assert_array_equal(expected_indices, indices)
+
+
+def test_spine_table_for_section(spines):
+    spine_table = spines.spine_table_for_section(2)
+
+    assert isinstance(spine_table, pd.DataFrame)
+    assert len(spine_table.columns) == 20
+    assert set(
+        [
+            "afferent_surface_x",
+            "afferent_center_x",
+            "spine_length",
+            "spine_orientation_vector_x",
+            "spine_rotation_x",
+            "afferent_section_id",
+        ]
+    ).issubset(set(spine_table.columns))
+    assert spine_table.loc[0, "afferent_surface_x"] == np.float64(2.0)
+    assert spine_table.loc[0, "spine_length"] == np.float64(2.0)
+
+
+def test_spine_meshes_for_section(spines_meshes):
+    meshes = list(spines_meshes.spine_meshes_for_section(2))
+    expected_points = [
+        [2.0, 2.0, 2.0],
+        [3.0, 2.0, 4.0],
+        [2.0, 3.0, 4.0],
+        [1.0, 2.0, 4.0],
+        [2.0, 1.0, 4.0],
+    ]
+
+    assert len(meshes) == 2
+    assert isinstance(meshes[0], trimesh.Trimesh)
+    assert_allclose(meshes[0].vertices, expected_points)
+
+
+def test_compound_spine_mesh_for_section(spines_meshes):
+    mesh = spines_meshes.compound_spine_mesh_for_section(2)
+    expected_points = [5.0, 5.0, 5.0]
+
+    assert isinstance(mesh, trimesh.Trimesh)
+    assert len(mesh.vertices) == 9
+    assert_allclose(mesh.vertices[5], expected_points)
+
+
+def test_centered_spine_meshes_for_section(spines_meshes):
+    meshes = list(spines_meshes.centered_spine_meshes_for_section(2))
+    expected_points = [
+        [0.0, 0.0, -1.0],
+        [1.0, 0.0, 1.0],
+        [0.0, 1.0, 1.0],
+        [-1.0, 0.0, 1.0],
+        [0.0, -1.0, 1.0],
+    ]
+
+    assert len(meshes) == 2
+    assert isinstance(meshes[0], trimesh.Trimesh)
+    assert_allclose(meshes[0].vertices, expected_points)
+
+
+def test_compound_centered_spine_mesh_for_section(spines_meshes):
+    mesh = spines_meshes.compound_centered_spine_mesh_for_section(2)
+    expected_points = [0.0, 0.0, -2.0]
+
+    assert isinstance(mesh, trimesh.Trimesh)
+    assert len(mesh.vertices) == 9
+    assert_allclose(mesh.vertices[5], expected_points)
--- tests/unit/utils/test_morph_spine_loader.py
+++ tests/unit/utils/test_morph_spine_loader.py
@@ -14,12 +14,19 @@ from morph_spines.utils.morph_spine_loader import (
     _resolve_morphology_name,
     load_morphology_with_spines,
     load_soma,
+    load_spine_skeletons,
     load_spine_table,
 )
 
 SAMPLE_DATA_DIR = f"{Path(__file__).parent.parent}/data"
 SAMPLE_MORPH_WITH_SPINES_DATASET_FILE = f"{SAMPLE_DATA_DIR}/morph_with_spines_schema_v1.0.h5"
 MORPH_WITH_SPINES_ID = "neuron_0"
+
+SAMPLE_MORPH_WITH_SPINES_COLLECTION_FILE = (
+    f"{SAMPLE_DATA_DIR}/morph_with_spines_col_v1.0_2nrn_3col.h5"
+)
+COL_MORPH_IDS = ["neuron_0", "neuron_1"]
+COL_COLLECTION_IDS = ["coll_0", "coll_1", "coll_2"]
 
 
 def test__resolve_morphology_name_single():
@@ -255,15 +262,52 @@ def test_load_spine_table_invalid_with_version10(tmp_path):
         load_spine_table(str(f), f"{GRP_EDGES}")
 
 
+def test_load_spine_skeletons_edgecase_spine_dup():
+    df = load_spine_table(
+        SAMPLE_MORPH_WITH_SPINES_DATASET_FILE, f"{GRP_EDGES}/{MORPH_WITH_SPINES_ID}"
+    )
+    df.drop(index=0, inplace=True)
+    df = pd.concat([df, df], axis=0, ignore_index=True)
+    spines_skeletons = load_spine_skeletons(
+        SAMPLE_MORPH_WITH_SPINES_DATASET_FILE, MORPH_WITH_SPINES_ID, df
+    )
+    num_spines = df.shape[0]
+
+    assert num_spines == len(spines_skeletons.neurites)
+
+
+def test_load_spine_skeletons_edgecase_spine_missing():
+    df = load_spine_table(
+        SAMPLE_MORPH_WITH_SPINES_DATASET_FILE, f"{GRP_EDGES}/{MORPH_WITH_SPINES_ID}"
+    )
+    df.drop(index=0, inplace=True)
+    spines_skeletons = load_spine_skeletons(
+        SAMPLE_MORPH_WITH_SPINES_DATASET_FILE, MORPH_WITH_SPINES_ID, df
+    )
+    num_spines = df.shape[0]
+
+    assert num_spines == len(spines_skeletons.neurites)
+
+
 def test_load_soma(tmp_path):
     soma = load_soma(SAMPLE_MORPH_WITH_SPINES_DATASET_FILE, MORPH_WITH_SPINES_ID)
 
     assert isinstance(soma, Soma)
 
 
-def test_load_morphology_with_spines():
+def test_load_morphology_with_spines_full_morph():
     morph_with_spines = load_morphology_with_spines(
         SAMPLE_MORPH_WITH_SPINES_DATASET_FILE, spines_are_centered=False
     )
 
     assert isinstance(morph_with_spines, MorphologyWithSpines)
+
+
+def test_load_morphology_with_spines_from_collection():
+    morph_with_spines = load_morphology_with_spines(
+        SAMPLE_MORPH_WITH_SPINES_COLLECTION_FILE, morphology_name=COL_MORPH_IDS[0]
+    )
+    num_spines = 2 * len(COL_COLLECTION_IDS)
+
+    assert isinstance(morph_with_spines, MorphologyWithSpines)
+    assert num_spines == morph_with_spines.spines.spine_count
